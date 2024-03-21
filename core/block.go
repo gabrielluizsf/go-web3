d59c44dc688f383b3d0d7d6ca6c19ed73bd14206
gabrielluizsf <gabrielluizads123@gmail.com>
2024-03-21 14:40:44 -0300

    using Bytes function

--- core/block.go
+++ core/block.go
@@ -18,6 +18,13 @@ type Header struct {
 	Height        uint32
 }
 
+func (h *Header) Bytes() []byte {
+	buf := &bytes.Buffer{}
+	enc := gob.NewEncoder(buf)
+	enc.Encode(h)
+	return buf.Bytes()
+}
+
 type Block struct {
 	*Header
 	Transactions []Transaction
@@ -33,16 +40,20 @@ func NewBlock(h *Header, transactions []Transaction) *Block {
 	}
 }
 
-func (b *Block) Hash(hasher Hasher[*Block]) types.Hash {
+func (b *Block) AddTransaction(transaction *Transaction){
+	b.Transactions = append(b.Transactions, *transaction)
+}
+
+func (b *Block) Hash(hasher Hasher[*Header]) types.Hash {
 	if b.hash.IsZero() {
-		b.hash = hasher.Hash(b)
+		b.hash = hasher.Hash(b.Header)
 	}
 	return b.hash
 }
 
 func (b *Block) Sign(privKey crypto.PrivateKey) error {
-	sign, err := privKey.Sign(b.HeaderData())
-	if err != nil{
+	sign, err := privKey.Sign(b.Header.Bytes())
+	if err != nil {
 		return err
 	}
 	b.Validator = privKey.PublicKey()
@@ -50,12 +61,17 @@ func (b *Block) Sign(privKey crypto.PrivateKey) error {
 	return nil
 }
 
-func (b *Block) Verify() error{
-	if b.Signature == nil{
+func (b *Block) Verify() error {
+	if b.Signature == nil {
 		return errors.New("block has no signature")
 	}
-	if !b.Signature.Verify(b.Validator, b.HeaderData()){
+	if !b.Signature.Verify(b.Validator, b.Header.Bytes()) {
 		return errors.New("block has invalid signature")
+	}
+	for _, transaction := range b.Transactions{
+		if err := transaction.Verify(); err != nil{
+			return err
+		}
 	}
 	return nil
 }
@@ -66,11 +82,4 @@ func (b *Block) Decode(r io.Reader, dec Decoder[*Block]) error {
 
 func (b *Block) Encode(w io.Writer, enc Encoder[*Block]) error {
 	return enc.Encode(w, b)
-}
-
-func (b *Block) HeaderData() []byte{
-	buf := &bytes.Buffer{}
-	enc := gob.NewEncoder(buf)
-	enc.Encode(b.Header)
-	return buf.Bytes()
 }
