b22dd3e153f32a466f0188ec0c2b0dff474ac6fa
gabrielluizsf <gabrielluizads123@gmail.com>
2024-03-21 14:44:56 -0300

    changing block tests

--- core/block_test.go
+++ core/block_test.go
@@ -9,10 +9,35 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func randomBlock(height uint32) *Block {
+func TestHashBlock(t *testing.T) {
+	block := randomBlock(0, types.Hash{})
+	assert.NotNil(t, block)
+	assert.Nil(t, block.Sign(privateKey))
+	assert.NotNil(t, block.Signature)
+}
+
+func TestVerifyBlock(t *testing.T) {
+	block := randomBlock(0, types.Hash{})
+	assert.NotNil(t, block)
+
+	assert.Nil(t, block.Sign(privateKey))
+	assert.Nil(t, block.Verify())
+
+	otherBlock := randomBlock(0, types.Hash{})
+	otherBlock.Height = 100
+	assert.NotNil(t, otherBlock.Verify())
+}
+
+func TestAddTransaction(t *testing.T){
+	b := randomBlockWithSign(t, 0, types.Hash{})
+	b.AddTransaction(transaction)
+	assert.Equal(t, b.Transactions[1],  *transaction)
+}
+
+func randomBlock(height uint32, prevBlockHash types.Hash) *Block {
 	header := &Header{
 		Version:       1,
-		PrevBlockHash: types.RandomHash(),
+		PrevBlockHash: prevBlockHash,
 		Height:        height,
 		Timestamp:     time.Now().UnixNano(),
 	}
@@ -23,26 +48,7 @@ func randomBlock(height uint32) *Block {
 	if err != nil {
 		return nil
 	}
-	transactions := []Transaction{{Data: msg, PublicKey: publicKey, Signature: sign}}
+	transactions := []Transaction{{Data: msg, From: publicKey, Signature: sign}}
 
 	return NewBlock(header, transactions)
-}
-
-func TestHashBlock(t *testing.T) {
-	block := randomBlock(0)
-	assert.NotNil(t, block)
-	assert.Nil(t, block.Sign(privateKey))
-	assert.NotNil(t, block.Signature)
-}
-
-func TestVerifyBlock(t *testing.T){
-	block := randomBlock(0)
-	assert.NotNil(t, block)
-
-	assert.Nil(t, block.Sign(privateKey))
-	assert.Nil(t, block.Verify())
-
-	otherBlock := randomBlock(0)
-	otherBlock.Height = 100
-	assert.NotNil(t, otherBlock.Verify())
 }
