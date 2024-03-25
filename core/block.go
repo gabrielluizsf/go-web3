031aea0e4f6991d0957de4bac0cf2640e9bca42b
gabrielluizsf <gabrielluizads123@gmail.com>
2024-03-25 10:38:31 -0300

    block implemented

--- core/block.go
+++ core/block.go
@@ -2,9 +2,10 @@ package core
 
 import (
 	"bytes"
+	"crypto/sha256"
 	"encoding/gob"
-	"errors"
-	"io"
+	"fmt"
+	"time"
 
 	"github.com/gabrielluizsf/go-web3/crypto"
 	"github.com/gabrielluizsf/go-web3/types"
@@ -14,72 +15,124 @@ type Header struct {
 	Version       uint32
 	DataHash      types.Hash
 	PrevBlockHash types.Hash
-	Timestamp     int64
 	Height        uint32
+	Timestamp     int64
 }
 
 func (h *Header) Bytes() []byte {
 	buf := &bytes.Buffer{}
 	enc := gob.NewEncoder(buf)
 	enc.Encode(h)
+
 	return buf.Bytes()
 }
 
 type Block struct {
 	*Header
-	Transactions []Transaction
+
+	Transactions []*Transaction
 	Validator    crypto.PublicKey
 	Signature    *crypto.Signature
-	hash         types.Hash
+
+	// Cached version of the header hash
+	hash types.Hash
 }
 
-func NewBlock(h *Header, transactions []Transaction) *Block {
+func NewBlock(h *Header, transactions []*Transaction) (*Block, error) {
 	return &Block{
 		Header:       h,
 		Transactions: transactions,
-	}
+	}, nil
 }
 
-func (b *Block) AddTransaction(transaction *Transaction){
-	b.Transactions = append(b.Transactions, *transaction)
+func NewBlockFromPrevHeader(prevHeader *Header, transactions []*Transaction) (*Block, error) {
+	dataHash, err := CalculateDataHash(transactions)
+	if err != nil {
+		return nil, err
+	}
+
+	header := &Header{
+		Version:       1,
+		Height:        prevHeader.Height + 1,
+		DataHash:      dataHash,
+		PrevBlockHash: BlockHasher{}.Hash(prevHeader),
+		Timestamp:     time.Now().UnixNano(),
+	}
+
+	return NewBlock(header, transactions)
+}
+
+func (b *Block) AddTransaction(transaction *Transaction) {
+	b.Transactions = append(b.Transactions, transaction)
+	hash, _ := CalculateDataHash(b.Transactions)
+	b.DataHash = hash
+}
+
+func (b *Block) Sign(privKey crypto.PrivateKey) error {
+	sig, err := privKey.Sign(b.Header.Bytes())
+	if err != nil {
+		return err
+	}
+
+	b.Validator = privKey.PublicKey()
+	b.Signature = sig
+
+	return nil
+}
+
+func (b *Block) Verify() error {
+	if b.Signature == nil {
+		return fmt.Errorf("block has no signature")
+	}
+
+	if !b.Signature.Verify(b.Validator, b.Header.Bytes()) {
+		return fmt.Errorf("block has invalid signature")
+	}
+
+	for _, transaction := range b.Transactions {
+		if err := transaction.Verify(); err != nil {
+			return err
+		}
+	}
+
+	dataHash, err := CalculateDataHash(b.Transactions)
+	if err != nil {
+		return err
+	}
+
+	if dataHash != b.DataHash {
+		return fmt.Errorf("block (%s) has an invalid data hash", b.Hash(BlockHasher{}))
+	}
+
+	return nil
+}
+
+func (b *Block) Decode(dec Decoder[*Block]) error {
+	return dec.Decode(b)
+}
+
+func (b *Block) Encode(enc Encoder[*Block]) error {
+	return enc.Encode(b)
 }
 
 func (b *Block) Hash(hasher Hasher[*Header]) types.Hash {
 	if b.hash.IsZero() {
 		b.hash = hasher.Hash(b.Header)
 	}
+
 	return b.hash
 }
 
-func (b *Block) Sign(privKey crypto.PrivateKey) error {
-	sign, err := privKey.Sign(b.Header.Bytes())
-	if err != nil {
-		return err
-	}
-	b.Validator = privKey.PublicKey()
-	b.Signature = sign
-	return nil
-}
+func CalculateDataHash(transactions []*Transaction) (hash types.Hash, err error) {
+	buf := &bytes.Buffer{}
 
-func (b *Block) Verify() error {
-	if b.Signature == nil {
-		return errors.New("block has no signature")
-	}
-	if !b.Signature.Verify(b.Validator, b.Header.Bytes()) {
-		return errors.New("block has invalid signature")
-	}
-	for _, transaction := range b.Transactions{
-		if err := transaction.Verify(); err != nil{
-			return err
+	for _, transaction := range transactions {
+		if err = transaction.Encode(NewGobTransactionEncoder(buf)); err != nil {
+			return
 		}
 	}
-	return nil
-}
 
-func (b *Block) Decode(r io.Reader, dec Decoder[*Block]) error {
-	return dec.Decode(r, b)
-}
+	hash = sha256.Sum256(buf.Bytes())
 
-func (b *Block) Encode(w io.Writer, enc Encoder[*Block]) error {
-	return enc.Encode(w, b)
+	return
 }
--- core/block_test.go
+++ core/block_test.go
@@ -1,6 +1,7 @@
 package core
 
 import (
+	"bytes"
 	"testing"
 	"time"
 
@@ -9,46 +10,64 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestHashBlock(t *testing.T) {
-	block := randomBlock(0, types.Hash{})
-	assert.NotNil(t, block)
-	assert.Nil(t, block.Sign(privateKey))
-	assert.NotNil(t, block.Signature)
+func TestSignBlock(t *testing.T) {
+	privKey := crypto.GeneratePrivateKey()
+	b := randomBlock(t, 0, types.Hash{})
+
+	assert.Nil(t, b.Sign(privKey))
+	assert.NotNil(t, b.Signature)
 }
 
 func TestVerifyBlock(t *testing.T) {
-	block := randomBlock(0, types.Hash{})
-	assert.NotNil(t, block)
+	privKey := crypto.GeneratePrivateKey()
+	b := randomBlock(t, 0, types.Hash{})
 
-	assert.Nil(t, block.Sign(privateKey))
-	assert.Nil(t, block.Verify())
+	assert.Nil(t, b.Sign(privKey))
+	assert.Nil(t, b.Verify())
 
-	otherBlock := randomBlock(0, types.Hash{})
-	otherBlock.Height = 100
-	assert.NotNil(t, otherBlock.Verify())
+	otherPrivKey := crypto.GeneratePrivateKey()
+	b.Validator = otherPrivKey.PublicKey()
+	assert.NotNil(t, b.Verify())
+
+	b.Height = 100
+	assert.NotNil(t, b.Verify())
 }
 
-func TestAddTransaction(t *testing.T){
-	b := randomBlockWithSign(t, 0, types.Hash{})
-	b.AddTransaction(transaction)
-	assert.Equal(t, b.Transactions[1],  *transaction)
+func TestDecodeEncodeBlock(t *testing.T) {
+	b := randomBlock(t, 1, types.Hash{})
+	buf := &bytes.Buffer{}
+	assert.Nil(t, b.Encode(NewGobBlockEncoder(buf)))
+
+	bDecode := new(Block)
+	assert.Nil(t, bDecode.Decode(NewGobBlockDecoder(buf)))
+
+	assert.Equal(t, b.Header, bDecode.Header)
+
+	for i := 0; i < len(b.Transactions); i++ {
+		b.Transactions[i].hash = types.Hash{}
+		assert.Equal(t, b.Transactions[i], bDecode.Transactions[i])
+	}
+
+	assert.Equal(t, b.Validator, bDecode.Validator)
+	assert.Equal(t, b.Signature, bDecode.Signature)
 }
 
-func randomBlock(height uint32, prevBlockHash types.Hash) *Block {
+func randomBlock(t *testing.T, height uint32, prevBlockHash types.Hash) *Block {
+	privKey := crypto.GeneratePrivateKey()
+	transaction := randomTransactionWithSignature(t)
 	header := &Header{
 		Version:       1,
 		PrevBlockHash: prevBlockHash,
 		Height:        height,
 		Timestamp:     time.Now().UnixNano(),
 	}
-	privateKey := crypto.GeneratePrivateKey()
-	publicKey := privateKey.PublicKey()
-	msg := []byte("Hello World")
-	sign, err := privateKey.Sign(msg)
-	if err != nil {
-		return nil
-	}
-	transactions := []Transaction{{Data: msg, From: publicKey, Signature: sign}}
 
-	return NewBlock(header, transactions)
+	b, err := NewBlock(header, []*Transaction{transaction})
+	assert.Nil(t, err)
+	dataHash, err := CalculateDataHash(b.Transactions)
+	assert.Nil(t, err)
+	b.Header.DataHash = dataHash
+	assert.Nil(t, b.Sign(privKey))
+
+	return b
 }
