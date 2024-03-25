4d3c279a4c27896c2fdc5eae585c9d881b153a98
gabrielluizsf <gabrielluizads123@gmail.com>
2024-03-25 10:37:35 -0300

    changing Hash function implementencion

--- core/hasher.go
+++ core/hasher.go
@@ -1,7 +1,9 @@
 package core
 
 import (
+	"bytes"
 	"crypto/sha256"
+	"encoding/binary"
 
 	"github.com/gabrielluizsf/go-web3/types"
 )
@@ -12,7 +14,22 @@ type Hasher[T any] interface {
 
 type BlockHasher struct{}
 
-func (BlockHasher) Hash(h *Header) types.Hash {
-	hash := sha256.Sum256(h.Bytes())
-	return types.Hash(hash)
+func (BlockHasher) Hash(b *Header) types.Hash {
+	h := sha256.Sum256(b.Bytes())
+	return types.Hash(h)
+}
+
+type TransactionHasher struct{}
+
+// Hash will hash the whole bytes of the transaction no exception.
+func (TransactionHasher) Hash(transaction *Transaction) types.Hash {
+	buf := new(bytes.Buffer)
+
+	binary.Write(buf, binary.LittleEndian, transaction.Data)
+	binary.Write(buf, binary.LittleEndian, transaction.To)
+	binary.Write(buf, binary.LittleEndian, transaction.Value)
+	binary.Write(buf, binary.LittleEndian, transaction.From)
+	binary.Write(buf, binary.LittleEndian, transaction.Nonce)
+
+	return types.Hash(sha256.Sum256(buf.Bytes()))
 }
