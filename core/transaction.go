5a9a4ae564824129cb1d400e992f2fd39a3da0a7
gabrielluizsf <gabrielluizads123@gmail.com>
2024-03-25 10:40:47 -0300

    transaction implemented

--- core/transaction.go
+++ core/transaction.go
@@ -1,40 +1,99 @@
 package core
 
 import (
-	"errors"
+	"encoding/gob"
+	"fmt"
+	"math/rand"
 
 	"github.com/gabrielluizsf/go-web3/crypto"
+	"github.com/gabrielluizsf/go-web3/types"
 )
 
-type Transaction struct {
-	Data []byte
+type TransactionType byte
 
-	From crypto.PublicKey
+const (
+	TransactionTypeCollection TransactionType = iota // 0x0
+	TransactionTypeMint                              // 0x01
+)
+
+type CollectionTransaction struct {
+	Fee      int64
+	MetaData []byte
+}
+
+type MintTransaction struct {
+	Fee             int64
+	NFT             types.Hash
+	Collection      types.Hash
+	MetaData        []byte
+	CollectionOwner crypto.PublicKey
+	Signature       crypto.Signature
+}
+
+type Transaction struct {
+	// Only used for native NFT logic
+	TransactionInner any
+	// Any arbitrary data for the VM
+	Data      []byte
+	To        crypto.PublicKey
+	Value     uint64
+	From      crypto.PublicKey
 	Signature *crypto.Signature
+	Nonce     int64
+
+	// cached version of the Transaction data hash
+	hash types.Hash
 }
 
 func NewTransaction(data []byte) *Transaction {
 	return &Transaction{
-		Data: data,
+		Data:  data,
+		Nonce: rand.Int63n(1000000000000000),
 	}
 }
 
-func (t *Transaction) Sign(privKey crypto.PrivateKey) error {
-	sign, err := privKey.Sign(t.Data)
+func (transaction *Transaction) Hash(hasher Hasher[*Transaction]) types.Hash {
+	if transaction.hash.IsZero() {
+		transaction.hash = hasher.Hash(transaction)
+	}
+	return transaction.hash
+}
+
+func (transaction *Transaction) Sign(privKey crypto.PrivateKey) error {
+	hash := transaction.Hash(TransactionHasher{})
+	sig, err := privKey.Sign(hash.ToSlice())
 	if err != nil {
 		return err
 	}
-	t.From = privKey.PublicKey()
-	t.Signature = sign
+
+	transaction.From = privKey.PublicKey()
+	transaction.Signature = sig
+
 	return nil
 }
 
-func (t *Transaction) Verify() error {
-	if t.Signature == nil {
-		return errors.New("transaction has no signature")
+func (transaction *Transaction) Verify() error {
+	if transaction.Signature == nil {
+		return fmt.Errorf("transaction has no signature")
 	}
-	if !t.Signature.Verify(t.From, t.Data) {
-		return errors.New("invalid transaction signature")
+
+	hash := transaction.Hash(TransactionHasher{})
+	if !transaction.Signature.Verify(transaction.From, hash.ToSlice()) {
+		return fmt.Errorf("invalid transaction signature")
 	}
+
 	return nil
+}
+
+func (transaction *Transaction) Decode(dec Decoder[*Transaction]) error {
+	return dec.Decode(transaction)
+}
+
+func (transaction *Transaction) Encode(enc Encoder[*Transaction]) error {
+	return enc.Encode(transaction)
+}
+
+func init() {
+	gob.Register(CollectionTransaction{})
+	gob.Register(MintTransaction{})
 }
--- core/transaction_test.go
+++ core/transaction_test.go
@@ -1,29 +1,107 @@
 package core
 
 import (
+	"bytes"
 	"testing"
 
 	"github.com/gabrielluizsf/go-web3/crypto"
+	"github.com/gabrielluizsf/go-web3/types"
 	"github.com/stretchr/testify/assert"
 )
 
-var (
-	privateKey  = crypto.GeneratePrivateKey()
-	data        = []byte("Hello World")
-	transaction = NewTransaction(data)
-)
+func TestVerifyTransactionWithTamper(t *testing.T) {
+	transaction := NewTransaction(nil)
+
+	fromPrivKey := crypto.GeneratePrivateKey()
+	toPrivKey := crypto.GeneratePrivateKey()
+	hackerPrivKey := crypto.GeneratePrivateKey()
+
+	transaction.From = fromPrivKey.PublicKey()
+	transaction.To = toPrivKey.PublicKey()
+	transaction.Value = 666
+
+	assert.Nil(t, transaction.Sign(fromPrivKey))
+	transaction.hash = types.Hash{}
+
+	transaction.To = hackerPrivKey.PublicKey()
+
+	assert.NotNil(t, transaction.Verify())
+}
+
+func TestNFTTransaction(t *testing.T) {
+	collectionTransaction := CollectionTransaction{
+		Fee:      200,
+		MetaData: []byte("The beginning of a new collection"),
+	}
+
+	privKey := crypto.GeneratePrivateKey()
+	transaction := &Transaction{
+		TransactionInner: collectionTransaction,
+	}
+	transaction.Sign(privKey)
+	transaction.hash = types.Hash{}
+
+	buf := new(bytes.Buffer)
+	assert.Nil(t, transaction.Encode(NewGobTransactionEncoder(buf)))
+
+	transactionDecoded := &Transaction{}
+	assert.Nil(t, transactionDecoded.Decode(NewGobTransactionDecoder(buf)))
+	assert.Equal(t, transaction, transactionDecoded)
+}
+
+func TestNativeTransferTransaction(t *testing.T) {
+	fromPrivKey := crypto.GeneratePrivateKey()
+	toPrivKey := crypto.GeneratePrivateKey()
+	transaction := &Transaction{
+		To:    toPrivKey.PublicKey(),
+		Value: 666,
+	}
+
+	assert.Nil(t, transaction.Sign(fromPrivKey))
+}
 
 func TestSignTransaction(t *testing.T) {
-	assert.Nil(t, transaction.Sign(privateKey))
+	privKey := crypto.GeneratePrivateKey()
+	transaction := &Transaction{
+		Data: []byte("foo"),
+	}
+
+	assert.Nil(t, transaction.Sign(privKey))
 	assert.NotNil(t, transaction.Signature)
-	assert.Equal(t, transaction.From, privateKey.PublicKey())
 }
 
 func TestVerifyTransaction(t *testing.T) {
-	assert.Nil(t, transaction.Sign(privateKey))
+	privKey := crypto.GeneratePrivateKey()
+	transaction := &Transaction{
+		Data: []byte("foo"),
+	}
+
+	assert.Nil(t, transaction.Sign(privKey))
 	assert.Nil(t, transaction.Verify())
+
 	otherPrivKey := crypto.GeneratePrivateKey()
 	transaction.From = otherPrivKey.PublicKey()
 
 	assert.NotNil(t, transaction.Verify())
+}
+
+func TestTransactionEncodeDecode(t *testing.T) {
+	transaction := randomTransactionWithSignature(t)
+	buf := &bytes.Buffer{}
+	assert.Nil(t, transaction.Encode(NewGobTransactionEncoder(buf)))
+	transaction.hash = types.Hash{}
+
+	transactionDecoded := new(Transaction)
+	assert.Nil(t, transactionDecoded.Decode(NewGobTransactionDecoder(buf)))
+	assert.Equal(t, transaction, transactionDecoded)
+}
+
+func randomTransactionWithSignature(t *testing.T) *Transaction {
+	privKey := crypto.GeneratePrivateKey()
+	transaction := Transaction{
+		Data: []byte("foo"),
+	}
+	assert.Nil(t, transaction.Sign(privKey))
+
+	return &transaction
 }
