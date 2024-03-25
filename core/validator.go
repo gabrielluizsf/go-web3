0116c2f629620110cf6f18ca537ab74c1282f746
gabrielluizsf <gabrielluizads123@gmail.com>
2024-03-25 10:33:21 -0300

    changing the errors

--- core/validator.go
+++ core/validator.go
@@ -1,8 +1,11 @@
 package core
 
 import (
+	"errors"
 	"fmt"
 )
+
+var ErrBlockKnown = errors.New("block already known")
 
 type Validator interface {
 	ValidateBlock(*Block) error
@@ -18,11 +21,11 @@ func NewBlockValidator(bc *Blockchain) *BlockValidator {
 
 func (v *BlockValidator) ValidateBlock(b *Block) error {
 	if v.bc.HasBlock(b.Height) {
-		return fmt.Errorf("chain already existis contains block (%d) with hash (%s)", b.Height, b.Hash(BlockHasher{}))
+		return ErrBlockKnown
 	}
 
-	if b.Height != (v.bc.Height() + 1) {
-		return fmt.Errorf("block (%s) too high", b.Hash(BlockHasher{}))
+	if b.Height != v.bc.Height()+1 {
+		return fmt.Errorf("block (%s) with height (%d) is too high => current height (%d)", b.Hash(BlockHasher{}), b.Height, v.bc.Height())
 	}
 
 	prevHeader, err := v.bc.GetHeader(b.Height - 1)
@@ -31,7 +34,6 @@ func (v *BlockValidator) ValidateBlock(b *Block) error {
 	}
 
 	hash := BlockHasher{}.Hash(prevHeader)
-
 	if hash != b.PrevBlockHash {
 		return fmt.Errorf("the hash of the previous block (%s) is invalid", b.PrevBlockHash)
 	}
