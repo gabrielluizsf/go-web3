e5d3616f8f53875abc162e6209ddecdfa9c26ed7
gabrielluizsf <gabrielluizads123@gmail.com>
2024-03-25 10:43:45 -0300

    server implemented

--- network/server.go
+++ network/server.go
@@ -1,51 +1,479 @@
 package network
 
 import (
-	"log"
+	"bytes"
+	"encoding/gob"
+	"fmt"
+	"net"
+	"os"
+	"sync"
 	"time"
+
+	"github.com/gabrielluizsf/go-web3/api"
+	"github.com/gabrielluizsf/go-web3/core"
+	"github.com/gabrielluizsf/go-web3/crypto"
+	"github.com/gabrielluizsf/go-web3/types"
+	"github.com/go-kit/log"
 )
 
+var defaultBlockTime = 5 * time.Second
+
 type ServerOpts struct {
-	Transports []Transport
+	APIListenAddr string
+	SeedNodes     []string
+	ListenAddr    string
+	TCPTransport  *TCPTransport
+	ID            string
+	Logger        log.Logger
+	RPCDecodeFunc RPCDecodeFunc
+	RPCProcessor  RPCProcessor
+	BlockTime     time.Duration
+	PrivateKey    *crypto.PrivateKey
 }
 
 type Server struct {
+	TCPTransport *TCPTransport
+	peerCh       chan *TCPPeer
+
+	mu      sync.RWMutex
+	peerMap map[net.Addr]*TCPPeer
+
 	ServerOpts
-	rpcCh  chan RPC
-	quitCh chan struct{}
+	mempool         *TransactionPool
+	chain           *core.Blockchain
+	isValidator     bool
+	rpcCh           chan RPC
+	quitCh          chan struct{}
+	TransactionChan chan *core.Transaction
 }
 
-func NewServer(opts ServerOpts) *Server {
-	return &Server{
-		opts,
-		make(chan RPC),
-		make(chan struct{}, 1),
+func NewServer(opts ServerOpts) (*Server, error) {
+	if opts.BlockTime == time.Duration(0) {
+		opts.BlockTime = defaultBlockTime
+	}
+	if opts.RPCDecodeFunc == nil {
+		opts.RPCDecodeFunc = DefaultRPCDecodeFunc
+	}
+	if opts.Logger == nil {
+		opts.Logger = log.NewLogfmtLogger(os.Stderr)
+		opts.Logger = log.With(opts.Logger, "addr", opts.ID)
+	}
+
+	chain, err := core.NewBlockchain(opts.Logger, genesisBlock())
+	if err != nil {
+		return nil, err
+	}
+
+	// Channel being used to communicate between the JSON RPC server
+	// and the node that will process this message.
+	TransactionChan := make(chan *core.Transaction)
+
+	// Only boot up the API server if the config has a valid port number.
+	if len(opts.APIListenAddr) > 0 {
+		apiServerCfg := api.ServerConfig{
+			Logger:     opts.Logger,
+			ListenAddr: opts.APIListenAddr,
+		}
+		apiServer := api.NewServer(apiServerCfg, chain, TransactionChan)
+		go apiServer.Start()
+
+		opts.Logger.Log("msg", "JSON API server running", "port", opts.APIListenAddr)
+	}
+
+	peerCh := make(chan *TCPPeer)
+	tr := NewTCPTransport(opts.ListenAddr, peerCh)
+
+	s := &Server{
+		TCPTransport:    tr,
+		peerCh:          peerCh,
+		peerMap:         make(map[net.Addr]*TCPPeer),
+		ServerOpts:      opts,
+		chain:           chain,
+		mempool:         NewTransactionPool(1000),
+		isValidator:     opts.PrivateKey != nil,
+		rpcCh:           make(chan RPC),
+		quitCh:          make(chan struct{}, 1),
+		TransactionChan: TransactionChan,
+	}
+
+	s.TCPTransport.peerCh = peerCh
+
+	// If we dont got any processor from the server options, we going to use
+	// the server as default.
+	if s.RPCProcessor == nil {
+		s.RPCProcessor = s
+	}
+
+	if s.isValidator {
+		go s.validatorLoop()
+	}
+
+	return s, nil
+}
+
+func (s *Server) bootstrapNetwork() {
+	for _, addr := range s.SeedNodes {
+		fmt.Println("trying to connect to ", addr)
+
+		go func(addr string) {
+			conn, err := net.Dial("tcp", addr)
+			if err != nil {
+				fmt.Printf("could not connect to %+v\n", conn)
+				return
+			}
+
+			s.peerCh <- &TCPPeer{
+				conn: conn,
+			}
+		}(addr)
 	}
 }
 
 func (s *Server) Start() {
-	s.initTransports()
-	ticker := time.NewTicker(5 * time.Second)
+	s.TCPTransport.Start()
+
+	time.Sleep(time.Second * 1)
+
+	s.bootstrapNetwork()
+
+	s.Logger.Log("msg", "accepting TCP connection on", "addr", s.ListenAddr, "id", s.ID)
+
 free:
 	for {
 		select {
+		case peer := <-s.peerCh:
+			s.peerMap[peer.conn.RemoteAddr()] = peer
+
+			go peer.readLoop(s.rpcCh)
+
+			if err := s.sendGetStatusMessage(peer); err != nil {
+				s.Logger.Log("err", err)
+				continue
+			}
+
+			s.Logger.Log("msg", "peer added to the server", "outgoing", peer.Outgoing, "addr", peer.conn.RemoteAddr())
+
+		case transaction := <-s.TransactionChan:
+			if err := s.processTransaction(transaction); err != nil {
+				s.Logger.Log("process Transaction error", err)
+			}
+
 		case rpc := <-s.rpcCh:
-			log.Printf("%+v\n", rpc)
+			msg, err := s.RPCDecodeFunc(rpc)
+			if err != nil {
+				s.Logger.Log("RPC error", err)
+				continue
+			}
+
+			if err := s.RPCProcessor.ProcessMessage(msg); err != nil {
+				if err != core.ErrBlockKnown {
+					s.Logger.Log("error", err)
+				}
+			}
+
 		case <-s.quitCh:
 			break free
-		case time := <-ticker.C:
-			log.Printf("%v", time)
 		}
 	}
-	log.Println("Server shutdown")
+
+	s.Logger.Log("msg", "Server is shutting down")
 }
 
-func (s *Server) initTransports() {
-	for _, tr := range s.Transports {
-		go func(tr Transport) {
-			for rpc := range tr.Consume() {
-				s.rpcCh <- rpc
-			}
-		}(tr)
+func (s *Server) validatorLoop() {
+	ticker := time.NewTicker(s.BlockTime)
+
+	s.Logger.Log("msg", "Starting validator loop", "blockTime", s.BlockTime)
+
+	for {
+		fmt.Println("creating new block")
+
+		if err := s.createNewBlock(); err != nil {
+			s.Logger.Log("create block error", err)
+		}
+
+		<-ticker.C
 	}
+}
+
+func (s *Server) ProcessMessage(msg *DecodedMessage) error {
+	switch t := msg.Data.(type) {
+	case *core.Transaction:
+		return s.processTransaction(t)
+	case *core.Block:
+		return s.processBlock(t)
+	case *GetStatusMessage:
+		return s.processGetStatusMessage(msg.From, t)
+	case *StatusMessage:
+		return s.processStatusMessage(msg.From, t)
+	case *GetBlocksMessage:
+		return s.processGetBlocksMessage(msg.From, t)
+	case *BlocksMessage:
+		return s.processBlocksMessage(msg.From, t)
+	}
+
+	return nil
+}
+
+func (s *Server) processGetBlocksMessage(from net.Addr, data *GetBlocksMessage) error {
+	s.Logger.Log("msg", "received getBlocks message", "from", from)
+
+	var (
+		blocks    = []*core.Block{}
+		ourHeight = s.chain.Height()
+	)
+
+	if data.To == 0 {
+		for i := int(data.From); i <= int(ourHeight); i++ {
+			block, err := s.chain.GetBlock(uint32(i))
+			if err != nil {
+				return err
+			}
+
+			blocks = append(blocks, block)
+		}
+	}
+
+	blocksMsg := &BlocksMessage{
+		Blocks: blocks,
+	}
+
+	buf := new(bytes.Buffer)
+	if err := gob.NewEncoder(buf).Encode(blocksMsg); err != nil {
+		return err
+	}
+
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+
+	msg := NewMessage(MessageTypeBlocks, buf.Bytes())
+	peer, ok := s.peerMap[from]
+	if !ok {
+		return fmt.Errorf("peer %s not known", peer.conn.RemoteAddr())
+	}
+
+	return peer.Send(msg.Bytes())
+}
+
+func (s *Server) sendGetStatusMessage(peer *TCPPeer) error {
+	var (
+		getStatusMsg = new(GetStatusMessage)
+		buf          = new(bytes.Buffer)
+	)
+	if err := gob.NewEncoder(buf).Encode(getStatusMsg); err != nil {
+		return err
+	}
+
+	msg := NewMessage(MessageTypeGetStatus, buf.Bytes())
+	return peer.Send(msg.Bytes())
+}
+
+func (s *Server) broadcast(payload []byte) error {
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+	for netAddr, peer := range s.peerMap {
+		if err := peer.Send(payload); err != nil {
+			fmt.Printf("peer send error => addr %s [err: %s]\n", netAddr, err)
+		}
+	}
+
+	return nil
+}
+
+func (s *Server) processBlocksMessage(from net.Addr, data *BlocksMessage) error {
+	s.Logger.Log("msg", "received BLOCKS!!!!!!!!", "from", from)
+
+	for _, block := range data.Blocks {
+		if err := s.chain.AddBlock(block); err != nil {
+			s.Logger.Log("error", err.Error())
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (s *Server) processStatusMessage(from net.Addr, data *StatusMessage) error {
+	s.Logger.Log("msg", "received STATUS message", "from", from)
+
+	if data.CurrentHeight <= s.chain.Height() {
+		s.Logger.Log("msg", "cannot sync blockHeight to low", "ourHeight", s.chain.Height(), "theirHeight", data.CurrentHeight, "addr", from)
+		return nil
+	}
+
+	go s.requestBlocksLoop(from)
+
+	return nil
+}
+
+func (s *Server) processGetStatusMessage(from net.Addr, data *GetStatusMessage) error {
+	s.Logger.Log("msg", "received getStatus message", "from", data, from)
+
+	statusMessage := &StatusMessage{
+		CurrentHeight: s.chain.Height(),
+		ID:            s.ID,
+	}
+
+	buf := new(bytes.Buffer)
+	if err := gob.NewEncoder(buf).Encode(statusMessage); err != nil {
+		return err
+	}
+
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+
+	peer, ok := s.peerMap[from]
+	if !ok {
+		return fmt.Errorf("peer %s not known", peer.conn.RemoteAddr())
+	}
+
+	msg := NewMessage(MessageTypeStatus, buf.Bytes())
+
+	return peer.Send(msg.Bytes())
+}
+
+func (s *Server) processBlock(b *core.Block) error {
+	if err := s.chain.AddBlock(b); err != nil {
+		s.Logger.Log("error", err.Error())
+		return err
+	}
+
+	go s.broadcastBlock(b)
+
+	return nil
+}
+
+func (s *Server) processTransaction(transaction *core.Transaction) error {
+	hash := transaction.Hash(core.TransactionHasher{})
+
+	if s.mempool.Contains(hash) {
+		return nil
+	}
+
+	if err := transaction.Verify(); err != nil {
+		return err
+	}
+	go s.broadcastTransaction(transaction)
+
+	s.mempool.Add(transaction)
+
+	return nil
+}
+
+// TODO: Find a way to make sure we dont keep syncing when we are at the highest
+// block height in the network.
+func (s *Server) requestBlocksLoop(peer net.Addr) error {
+	ticker := time.NewTicker(3 * time.Second)
+
+	for {
+		ourHeight := s.chain.Height()
+
+		s.Logger.Log("msg", "requesting new blocks", "requesting height", ourHeight+1)
+
+		// In this case we are 100% sure that the node has blocks heigher than us.
+		getBlocksMessage := &GetBlocksMessage{
+			From: ourHeight + 1,
+			To:   0,
+		}
+
+		buf := new(bytes.Buffer)
+		if err := gob.NewEncoder(buf).Encode(getBlocksMessage); err != nil {
+			return err
+		}
+
+		s.mu.RLock()
+		defer s.mu.RUnlock()
+
+		msg := NewMessage(MessageTypeGetBlocks, buf.Bytes())
+		peer, ok := s.peerMap[peer]
+		if !ok {
+			return fmt.Errorf("peer %s not known", peer.conn.RemoteAddr())
+		}
+
+		if err := peer.Send(msg.Bytes()); err != nil {
+			s.Logger.Log("error", "failed to send to peer", "err", err, "peer", peer)
+		}
+
+		<-ticker.C
+	}
+}
+
+func (s *Server) broadcastBlock(b *core.Block) error {
+	buf := &bytes.Buffer{}
+	if err := b.Encode(core.NewGobBlockEncoder(buf)); err != nil {
+		return err
+	}
+
+	msg := NewMessage(MessageTypeBlock, buf.Bytes())
+
+	return s.broadcast(msg.Bytes())
+}
+
+func (s *Server) broadcastTransaction(transaction *core.Transaction) error {
+	buf := &bytes.Buffer{}
+	if err := transaction.Encode(core.NewGobTransactionEncoder(buf)); err != nil {
+		return err
+	}
+
+	msg := NewMessage(MessageTypeTransaction, buf.Bytes())
+
+	return s.broadcast(msg.Bytes())
+}
+
+func (s *Server) createNewBlock() error {
+	currentHeader, err := s.chain.GetHeader(s.chain.Height())
+	if err != nil {
+		return err
+	}
+
+	// For now we are going to use all transactions that are in the pending pool
+	// Later on when we know the internal structure of our transaction
+	// we will implement some kind of complexity function to determine how
+	// many transactions can be included in a block.
+	transactions := s.mempool.Pending()
+
+	block, err := core.NewBlockFromPrevHeader(currentHeader, transactions)
+	if err != nil {
+		return err
+	}
+
+	if err := block.Sign(*s.PrivateKey); err != nil {
+		return err
+	}
+
+	if err := s.chain.AddBlock(block); err != nil {
+		return err
+	}
+
+	// TODO(@anthdm): pending pool of Transaction should only reflect on validator nodes.
+	// Right now "normal nodes" does not have their pending pool cleared.
+	s.mempool.ClearPending()
+
+	go s.broadcastBlock(block)
+
+	return nil
+}
+
+func genesisBlock() *core.Block {
+	header := &core.Header{
+		Version:   1,
+		DataHash:  types.Hash{},
+		Height:    0,
+		Timestamp: 000000,
+	}
+
+	b, _ := core.NewBlock(header, nil)
+
+	coinbase := crypto.PublicKey{}
+	transaction := core.NewTransaction(nil)
+	transaction.From = coinbase
+	transaction.To = coinbase
+	transaction.Value = 10_000_000
+	b.Transactions = append(b.Transactions, transaction)
+
+	privKey := crypto.GeneratePrivateKey()
+	if err := b.Sign(privKey); err != nil {
+		panic(err)
+	}
+
+	return b
 }
